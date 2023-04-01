7fe9809ab173c0f1dca7ddb3644a4afdc411637b
chris bell <elmntwzard@gmail.com>
2023-04-01 15:25:00 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1= 2;
+let num2= 31;
+let multiply= num1* num2
 
-// Write your code here
+let random = 4
+Math.floor(Math.random() * 3) + 1;
+
+let num3= 16;
+let num4= 6;
+let mod= num3%num4;
+console.log(mod)
+
+const max= 20;
